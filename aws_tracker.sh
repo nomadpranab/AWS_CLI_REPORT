d20b961ad967ccbd17fb6972d007a6a4846f9d74
root <root@ip-172-31-23-125.ec2.internal>
2026-03-21 09:22:11 +0000

    Added aws_tracker.sh script

---
+++ aws_tracker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+
+############
+#Author : Pranab
+#Date : 21-03
+#Version: v1
+#
+#This script will report the aws resources usage.
+#	2. The script should report usage of:
+#                EC2 → running/stopped instances
+#		 S3 → number of buckets and size (if possible)
+#		 RDS → database instances
+#                Lambda Functions
+#
+#
+##################################################
+#
+#List S3 buckets
+echo "List of S3 buckets"
+aws s3 ls
+
+#list EC2 Instance
+echo "List of EC2 Instance ID that are running"
+
+aws ec2 describe-instances --filter "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].{ID: InstanceId}" --output table
+#List Lamba Functions
+echo "Print list of lambda functions"
+aws lambda list-functions
+
+#List IAM Users
+echo "Print Ia, users"
+aws iam list-users
